39a67ef19172df7af7fab1c2b5fc9d8393a350c3
rivacortezf <u202215313@upc.edu.pe>
2024-07-08 11:01:28 -0500

    added middleware

---
+++ learning-center-platformRI/IAM/Infrastructure/Pipeline/Middleware/Attributes/AllowAnonymousAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace learning_center_platformRI.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+/**
+ * This attribute is used to decorate controllers and actions that do not require authorization.
+ * It skips authorization if action is decorated with [AllowAnonymous] attribute.
+ */
+[AttributeUsage(AttributeTargets.Method)]
+public class AllowAnonymousAttribute : Attribute
+{
+}
---
+++ learning-center-platformRI/IAM/Infrastructure/Pipeline/Middleware/Extensions/RequestAuthorizationMiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using learning_center_platformRI.IAM.Infrastructure.Pipeline.Middleware.Components;
+
+namespace learning_center_platformRI.IAM.Infrastructure.Pipeline.Middleware.Extensions;
+
+/**
+ * RequestAuthorizationMiddlewareExtensions
+ * This class includes a method extension to register RequestAuthorizationMiddleware in the ASP.NET Core pipeline.
+ */
+public static class RequestAuthorizationMiddlewareExtensions
+{
+    /**
+     * UseRequestAuthorization extension method is used to register RequestAuthorizationMiddleware in the ASP.NET Core pipeline.
+     */
+    public static IApplicationBuilder UseRequestAuthorization(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<RequestAuthorizationMiddleware>();
+    }
+}
